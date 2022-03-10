6596a02a55e6896024f96c91a2efae7bbc49f2db
RafaelRodrigues92 <101186325+RafaelRodrigues92@users.noreply.github.com>
2022-03-10 11:06:03 -0300

    carrossel + style

---
+++ script.js
@@ -0,0 +1,31 @@
+const glider = new Glider(document.getElementById('glider'), {
+    slidesToScroll: 1,
+  slidesToShow: 1
+});
+
+function sliderAuto(slider, miliseconds) {
+ const slidesCount = slider.track.childElementCount;
+ let slideTimeout = null;
+ let nextIndex = 1;
+
+ function slide () {
+   slideTimeout = setTimeout(
+     function () {
+       if (nextIndex >= slidesCount ) {
+         nextIndex = 0;
+       }
+       slider.scrollItem(nextIndex++);
+     },
+     miliseconds
+   );
+ }
+
+ slider.ele.addEventListener('glider-animated', function() {
+   window.clearInterval(slideTimeout);
+   slide();
+ });
+
+ slide();
+}
+
+sliderAuto(glider, 1000)

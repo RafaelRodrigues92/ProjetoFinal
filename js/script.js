c0bbd8e5ff9cc15f81273f0b19169dac5233bc76
RafaelRodrigues92 <101186325+RafaelRodrigues92@users.noreply.github.com>
2022-03-13 22:43:46 -0300

    gsap

---
+++ js/script.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".animate-inicial", {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.4
+});
+gsap.from(".img-dir", {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.4
+});
+
+
+gsap.from(".animate-inicial-img", {
+    duration: 0.9,
+    opacity: 0,
+    x: 250,
+});
+
+gsap.from(".animate-artigos", {
+    duration: 0.8,
+    opacity: 0,
+    x: -100,
+    stagger: 0.2,
+    delay: 1,
+});
+
+gsap.from(".animate-final", {
+    scrollTrigger: ".animate-final",
+    duration: 0.8,
+    opacity: 0,
+    y: -200,
+    stagger: 0.4,
+    delay: 0.5,
+});
+
